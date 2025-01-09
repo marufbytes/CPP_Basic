bf6da18e49d41c4da0357b57994fd34cce1001d2
Maruf Ahammed <marufahammed215@gmail.com>
2025-01-10 01:36:16 +0600

    Dynamic Allocation

---
+++ Queue_DS_Lab/Dynamic.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n = 5;
+int start = -1, rear = -1;
+int *Queue = new int[n];
+
+bool isEmpty()
+{
+    return start == -1 || start == rear + 1;
+}
+
+bool isFull()
+{
+    return rear == n - 1;
+}
+
+void enQueue(int data)
+{
+    if (isFull())
+    {
+        cout<<"Fulll\n";
+    }
+    if (start == -1)
+        start = 0;
+    rear++;
+    Queue[rear] = data;
+}
+
+int deQueue()
+{
+    if (isEmpty())
+    {
+        printf("\n Queue is Empty ! \n");
+        return 0;
+    }
+    int member = Queue[start];
+    start = start + 1;
+    return member;
+}
+
+void display()
+{
+    if (start == -1 || start == rear + 1)
+    {
+        cout << "\nEmpty\n";
+        return;
+    }
+    for (int i = start; i <= rear; i++)
+    {
+        cout << Queue[i] << "    ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    enQueue(10);
+    enQueue(20);
+    enQueue(30);
+    enQueue(40);
+    enQueue(50);
+    enQueue(60);
+
+    display();
+
+    cout << deQueue() << endl;
+
+    display();
+
+    cout << deQueue() << endl;
+    cout << deQueue() << endl;
+    cout << deQueue() << endl;
+    cout << deQueue() << endl;
+    cout << deQueue() << endl;
+
+    display();
+
+    return 0;
+}
