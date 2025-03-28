f745b3d22ea6434b03be8742ad5a96ca7fb70808
Maruf Ahammed <marufahammed215@gmail.com>
2025-03-28 10:31:49 +0600

    Update Merge_Sort.cpp

--- Sorting/Merge_Sort.cpp
+++ Sorting/Merge_Sort.cpp
@@ -5,22 +5,24 @@ void mergeSort(int array[], int l, int h);
 
 int main()
 {
-    int array[] = {3, 5, 67, 4, 2, 34, 7, 87, 56, 34, 56, 233, 78, 45};
+    int array[] = {4, 1, 20, 12, 11, 9, 5};
     int size = sizeof(array) / sizeof(array[0]);
 
-    mergeSort(array, 0, size - 1); 
-
+    mergeSort(array, 0, size - 1);
     for (int i = 0; i < size; i++)
     {
         cout << array[i] << "  ";
     }
+
     return 0;
 }
 
 void mergeSort(int array[], int l, int h)
 {
     if (l >= h)
+    {
         return;
+    }
 
     int mid = l + (h - l) / 2;
 
@@ -35,19 +37,30 @@ void mergeSort(int array[], int l, int h)
     while (i <= mid && j <= h)
     {
         if (array[i] <= array[j])
+        {
             temp[k++] = array[i++];
+        }
         else
+        {
             temp[k++] = array[j++];
+        }
     }
 
+    // copying remaining elements from left half
     while (i <= mid)
+    {
         temp[k++] = array[i++];
+    }
 
+    // copying remaining elements from right half
     while (j <= h)
+    {
         temp[k++] = array[j++];
+    }
 
+    // copying sorted elements back to original array
     for (int m = 0; m < k; m++)
-        array[l + m] = temp[m];    
+    {
+        array[l + m] = temp[m];
+    }
 }
-
-
