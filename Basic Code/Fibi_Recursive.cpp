90bd706483ee03b462e62deb4c50c14378a3194e
Maruf Ahammed <marufahammed215@gmail.com>
2025-05-15 01:50:13 +0600

    Create Fibi_Recursive.cpp

---
+++ Basic Code/Fibi_Recursive.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// recursive function to return nth fibonacci number
+int fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "enter the number of terms: ";
+    cin >> n;
+
+    cout << "fibonacci series: ";
+    for (int i = 0; i < n; ++i) {
+        cout << fibonacci(i) << " ";
+    }
+
+    return 0;
+}
